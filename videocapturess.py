3d60a77ea95a06946590fbccfb4a24460c1c15c4
Deepanshu Kushwaha <56316738+Deep2631@users.noreply.github.com>
2021-04-30 10:56:58 +0530

    Update videocapturess.py

--- videocapturess.py
+++ videocapturess.py
@@ -1,5 +1,11 @@
 import cv2
 cap = cv2.VideoCapture(0)#capture video and store in variable
+#we can get the default webcam resolution using get method
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HRIGHT))
+    # but we can set the max resolution using set method
+cap.set(3,1280)
+cap.set(4,720)
 while(True):# run continuously
     ret ,  frame = cap.read()# read capture data form cap to frame
 
